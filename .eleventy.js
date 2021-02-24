0323e310728c44f89272fb8f6809d0e76557c14f
jacobjavits <jacobcmckelvie@gmail.com>
2021-02-23 22:24:59 -0500

    added eleventy image plugin

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,25 @@
 const eleventyVue = require("@11ty/eleventy-plugin-vue");
+const Image = require("@11ty/eleventy-img");
+
+async function imageShortcode(src, cls, alt, sizes) {
+  let metadata = await Image(src, {
+    widths: [300],
+    formats: ["avif", "webp", "jpeg"],
+    outputDir: "./_site/assets/images",
+    urlPath: "/assets/images/",
+  });
+
+  let imageAttributes = {
+    alt,
+    sizes,
+    class: cls,
+    loading: "lazy",
+    decoding: "async",
+  };
+
+  // You bet we throw an error on missing alt in `imageAttributes` (alt="" works okay)
+  return Image.generateHTML(metadata, imageAttributes, {whitespaceMode: "inline"});
+}
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.setTemplateFormats([
@@ -13,6 +34,8 @@ module.exports = function(eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy("assets/fonts/*.woff");
   eleventyConfig.addPassthroughCopy("assets/fonts/*.woff2");
+
+  eleventyConfig.addNunjucksAsyncShortcode("image", imageShortcode);
 
   eleventyConfig.addPlugin(eleventyVue);
 };
